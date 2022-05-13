22fb60bf6b5cb1f42e72db17ea488c18751c896a
unknown <james.yoo715@gmail.com>
2022-05-12 23:59:58 -0400

    bsaic setup for game logic

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QLabel
+from PyQt5.QtCore import Qt
 
 class MineSweeperUI(QMainWindow):
     def __init__(self):
@@ -34,6 +35,7 @@ class MineSweeperUI(QMainWindow):
         # set the layout for the main menu
         self.current_screen.setLayout(self.layout)
 
+    # easy difficulty when selected
     def PlayEasy(self):
         self.gameLength = 9
         self.gameWidth = 9
@@ -42,6 +44,7 @@ class MineSweeperUI(QMainWindow):
         self.layout.addWidget(play)
         self.setCentralWidget(play)
     
+    # medium difficulty when selected
     def PlayMedium(self):
         self.gameLength = 16
         self.gameWidth = 16
@@ -50,6 +53,7 @@ class MineSweeperUI(QMainWindow):
         self.layout.addWidget(play)
         self.setCentralWidget(play)
 
+    # Hard difficulty when selected
     def PlayHard(self):
         self.gameLength = 16
         self.gameWidth = 30
@@ -62,14 +66,17 @@ class MineSweeperUI(QMainWindow):
 class PlayWidget(QWidget):
     def __init__(self, parent=None):
         super(PlayWidget, self).__init__(parent)
+        # options for user to select a level
         self.easy = QPushButton('Play 9X9 Minsweeper')
         self.medium = QPushButton('Play 16X16 Minesweeper')
         self.hard = QPushButton('Play 30X16 Minesweeper')
 
+        # add widget to screen
         parent.layout.addWidget(self.easy)
         parent.layout.addWidget(self.medium)
         parent.layout.addWidget(self.hard)
 
+        # when clicked set up game
         self.easy.clicked.connect(self.parent().PlayEasy) 
         self.medium.clicked.connect(self.parent().PlayMedium) 
         self.hard.clicked.connect(self.parent().PlayHard) 
@@ -79,11 +86,23 @@ class PlayWidget(QWidget):
 class PlayPage(QWidget):
     def __init__(self, parent=None):
         super(PlayPage, self).__init__(parent)
-        layout = QHBoxLayout()
-        self.label = QLabel('')
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+        # basic details about the mines and board size
+        self.mines = parent.mines 
+        self.gameLength = parent.gameLength
+        self.gameWidth = parent.gameWidth
 
+        layout = QHBoxLayout()
+        window = QWidget()
+
+        # label for the remaining mines
+        self.totalMines = QLabel()
+        self.totalMines.setAlignment(Qt.AlignCenter)
+        
+        self.totalMines.setText("mines")
+
+        layout.addWidget(self.totalMines)
+
+        
 def main():
     # instance of class
     mineInstance = QApplication(sys.argv)
